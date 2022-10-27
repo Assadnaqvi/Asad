8f85eafec9e44166a7f6c52be9873fff160c14ea
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-10-27 21:14:33 +0500

    Add files via upload

---
+++ lower upper title case.js
@@ -0,0 +1,12 @@
+  let name="asad naqvi";
+let name1  =  name .toUpperCase();
+  console.log(name1)
+let name2="ASAD NAQVI";
+let name3=name2.toLowerCase();
+console.log(name3);
+
+let name4="asad";
+let name5=name4.toTitleCase();
+console.log(name5);
+
+  
