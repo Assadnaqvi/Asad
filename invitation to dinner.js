e8d90f652c7697c6d73ee6264d1c7985a30efca7
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-10-27 23:52:23 +0500

    Add files via upload

---
+++ invitation to dinner.js
@@ -0,0 +1,7 @@
+let guests=["Ali","Asad","Ejaz"];
+console.log(guests);
+let invitation="i would like to invite you at dinner ";
+console.log(invitation);
+console.log(invitation+guests[0]);
+console.log(invitation+guests[1]);
+console.log(invitation+guests[2]);
