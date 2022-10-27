692caf928634c439b0f2ab115ac8680c16a5afd1
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-10-28 00:17:55 +0500

    Add files via upload

---
+++ invitation to dinner change a person.js
@@ -0,0 +1,10 @@
+let guests=["Ali","Asad","Ejaz"];
+
+
+guests[0]="Hussain";
+console.log(guests[0]);
+let invitation="i would like to invite you at dinner ";
+console.log(invitation);
+console.log(invitation+guests[0]);
+console.log(invitation+guests[1]);
+console.log(invitation+guests[2]);
