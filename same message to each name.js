a91c94d30e7bbfee75e8db8578114636a6062157
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-10-27 22:09:12 +0500

    Add files via upload

---
+++ same message to each name.js
@@ -0,0 +1,16 @@
+let names=["Asad","yasir","Ali","Hussain","Ejaz","Adeel"];
+let friends=["my friends are my life"];
+console.log(names);
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[4]);
+console.log(names[5]);
+console.log(friends+ " =!Asad");
+console.log(friends+ " =!yasir");
+console.log(friends+ " =!Ali");
+console.log(friends+ " =!Hussain");
+console.log(friends+ " =!Ejaz");
+console.log(friends+ " =!Adeel");
+
