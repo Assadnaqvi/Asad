93f87ee9631991433ec33242b0c4dca03bbc1e05
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-10-27 21:16:36 +0500

    Add files via upload

---
+++ stripping name.js
@@ -0,0 +1,8 @@
+let name="    Assad Naqvi    ";
+let name1=(name);
+console.log(name1);
+console.log(name1.length);
+let namea= "   Assad Naqvi    ";
+let nameb=namea.trim();
+console.log(nameb);
+console.log(nameb.length);
