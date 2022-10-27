7c06ac256f15fa85276b64dcb4e483c715f4d718
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-10-27 21:13:02 +0500

    Add files via upload

---
+++ adding comments.js
@@ -0,0 +1,8 @@
+//store name in variable and print it
+let name="Assad naqvi";
+console.log("my name is = "+name);
+//store two numbers in variables and do add operation 
+let a=2;
+let b=4;
+let c=a+b;
+console.log(c);
