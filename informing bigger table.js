b9ed8102a67956c3929526ade2b7893661bc0d35
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-10-28 22:37:35 +0500

    Add files via upload

---
+++ informing bigger table.js
@@ -0,0 +1,16 @@
+let guests=["Ali","Asad","Ejaz"];
+let guests1=["Hassan","Zawar","Mohsan"];
+
+console.log(guests1);
+console.log(guests);
+console.log(guests.concat(guests1));
+let invitation="i would like to invite you at dinner ";
+console.log(invitation);
+console.log(invitation+guests[0]);
+console.log(invitation+guests[1]);
+console.log(invitation+guests[2]);
+//console.log(invitation);
+console.log(invitation+guests1[0]);
+console.log(invitation+guests1[1]);
+console.log(invitation+guests1[2]);
+console.log("i would like to inform you that i have arranged a bigger table for dinner");
