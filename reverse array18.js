baa3a38c9920fda648ea00b2ec56d6adf9363e09
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-11-08 23:43:47 +0500

    Add files via upload

---
+++ reverse array18.js
@@ -0,0 +1,5 @@
+let countries=["Pakistan","Iran","England","Canada","America"];
+console.log(countries);
+console.log(countries.sort());
+console.log(countries.reverse());
+console.log(countries.reverse());
