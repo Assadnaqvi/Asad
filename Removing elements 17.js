2620ec662a5c7f1a845114d2d818a783a5e72746
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-11-08 23:42:52 +0500

    Add files via upload

---
+++ Removing elements 17.js
@@ -0,0 +1,16 @@
+let guests=["Ali","Asad","Ejaz","Hassan","Zawar","Mohsan"];
+console.log(guests);
+guests.pop();
+console.log();
+console.log("i can not invite you at dinner Mohsan");
+
+console.log(guests);
+guests.pop();
+console.log("i can not invite you at dinner Zawar");
+console.log(guests);
+guests.pop();
+console.log("i can not invite you at dinner Hassan");
+console.log(guests);
+guests.pop();
+console.log("i can not invite you at dinner Ejaz");
+console.log(guests);
