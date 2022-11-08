53e5115835f92720a91147894602830f68b45b4c
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-11-08 23:40:36 +0500

    Add files via upload

---
+++ conditional tests24.js
@@ -0,0 +1,305 @@
+let car="honda";
+
+if (car=="honda"){
+
+    console.log("if car=='honda',I pridict True");
+    
+    }else {
+    
+    console.log(car=="honda");
+    }
+ 
+ 
+    if (car!=="honda"){
+
+        console.log("if car=='honda',I pridict True");
+        
+        }else {
+        
+        console.log(car=="honda");
+        }
+        if (car!=="honda"){
+
+            console.log("if car=='honda',I pridict True");
+            
+            }else {
+            
+            console.log(car!=="honda");
+            }
+            
+    if (car!=="honda"){
+
+        console.log("if car=='honda',I pridict True");
+        
+        }else {
+        
+        console.log(car=="honda");
+        }
+        if (car!=="honda"){
+
+            console.log("if car=='honda',I pridict True");
+            
+            }else {
+            
+            console.log(car!=="honda");
+            }
+            
+    if (car!=="honda"){
+
+        console.log("if car=='honda',I pridict True");
+        
+        }else {
+        
+        console.log(car=="honda");
+        }
+        if (car!=="honda"){
+
+            console.log("if car=='honda',I pridict True");
+            
+            }else {
+            
+            console.log(car!=="honda");
+            }
+            
+    if (car!=="honda"){
+
+        console.log("if car=='honda',I pridict True");
+        
+        }else {
+        
+        console.log(car=="honda");
+        }
+        if (car!=="honda"){
+
+            console.log("if car=='honda',I pridict True");
+            
+            }else {
+            
+            console.log(car!=="honda");
+            }
+            
+    if (car!=="honda"){
+
+        console.log("if car=='honda',I pridict True");
+        
+        }else {
+        
+        console.log(car=="honda");
+        }
+        if (car!=="honda"){
+
+            console.log("if car=='honda',I pridict True");
+            
+            }else {
+            
+            console.log(car!=="honda");
+            }
+          
+
+ 
+ 
+    if (car!=="honda"){
+
+        console.log("if car=='honda',I pridict True");
+        
+        }else {
+        
+        console.log(car=="honda");
+        }
+        if (car!=="honda"){
+
+            console.log("if car=='honda',I pridict True");
+            
+            }else {
+            
+            console.log(car!=="honda");
+            }
+            
+    if (car!=="honda"){
+
+        console.log("if car=='honda',I pridict True");
+        
+        }else {
+        
+        console.log(car=="honda");
+        }
+        if (car!=="honda"){
+
+            console.log("if car=='honda',I pridict True");
+            
+            }else {
+            
+            console.log(car!=="honda");
+            }
+            
+    if (car!=="honda"){
+
+        console.log("if car=='honda',I pridict True");
+        
+        }else {
+        
+        console.log(car=="honda");
+        }
+        if (car!=="honda"){
+
+            console.log("if car=='honda',I pridict True");
+            
+            }else {
+            
+            console.log(car!=="honda");
+            }
+            
+    if (car!=="honda"){
+
+        console.log("if car=='honda',I pridict True");
+        
+        }else {
+        
+        console.log(car=="honda");
+        }
+        if (car!=="honda"){
+
+            console.log("if car=='honda',I pridict True");
+            
+            }else {
+            
+            console.log(car!=="honda");
+            }
+            
+    if (car!=="honda"){
+
+        console.log("if car=='honda',I pridict True");
+        
+        }else {
+        
+        console.log(car=="honda");
+        }
+        if (car!=="honda"){
+
+            console.log("if car=='honda',I pridict True");
+            
+            }else {
+            
+            console.log(car!=="honda");
+            }
+          if (car=="honda"){
+
+        console.log("its the equality of function");
+          }
+          else{
+            console.log("its not equality of function");
+          }
+          if (car=="toyota"){
+            console.log("its the equality of the function");
+          }else{
+            console.log("strings are not same");
+          }
+          if (car="honda"){
+            console.log("its the lower case function");
+          }else{
+            console.log("its the upper case function");
+
+          }
+          {
+         console.log("numbers are not equal");
+         }
+          let a=10;
+          b=20;
+        
+if (a=10,b=20){
+    console.log("a=10,b=20");
+}else {
+    console.log("numbers are not equal");
+}
+let a1=10;
+b1=20;
+
+if (a1=20,b1=20){
+console.log("a=10,b=20");
+}else {
+console.log("numbers are not equal");
+}
+
+
+let age=20;
+cost=0;
+package=0;
+if (age<30 && age>10){
+    cost=0;
+    package="if age<30 and >10 ,Net package is free for under 20";
+}
+else{
+    cost=2;
+    package ="net package is of cost 2 doller";
+}{console.log(package);
+console.log("cost is ="+cost);
+}
+let age1=20;
+cost=0;
+package=0;
+if (age1>=20){
+    cost=0;
+    package="if age is greater than and equal to 20,Net package is free for under 20";
+}
+else{
+    cost=2;
+    package ="net package is of cost 2 doller";
+}{console.log(package);
+console.log("cost is ="+cost);
+}
+let age2=20;
+cost=0;
+package=0;
+if (age2<=20){
+    cost=0;
+    package="if age is less than and equal to  20,Net package is free for under 20";
+}
+else{
+    cost=2;
+    package ="net package is of cost 2 doller";
+}{console.log(package);
+console.log("cost is ="+cost);
+}
+let age3=20;
+cost=0;
+package=0;
+if (age3<30 && age>10){
+    cost=0;
+    package="if age<30 and >10 ,Net package is free for under 20";
+}
+else{
+    cost=2;
+    package ="net package is of cost 2 doller";
+}{console.log(package);
+console.log("cost is ="+cost);
+}
+let age4=20;
+cost=0;
+package=0;
+if (age3<30 || age<=10){
+    cost=0;
+    package="if age<30 OR >10 ,Net package is free for under 20";
+}
+else{
+    cost=2;
+    package ="net package is of cost 2 doller";
+}{console.log(package);
+console.log("cost is ="+cost);
+}
+
+let grosary=["soap","toothpaste","milk","oil","suger"];
+if(grosary="soap"){
+    grosary="soap is found in the grosary";
+
+}else{
+grosary="soap is not found in the grosary";
+}
+console.log(grosary);
+let grossary=["soap","toothpaste","milk","oil","suger"];
+if(grossary=="salt"){
+    grossary="soap is found in the grosary";
+
+}else{
+grossary="soap is not found in the grosary";
+}
+console.log(grossary);
