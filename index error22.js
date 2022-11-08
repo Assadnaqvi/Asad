9a093f4513300f66cfc7d8ff1fe4e037571ef1a8
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-11-08 23:41:44 +0500

    Add files via upload

---
+++ index error22.js
@@ -0,0 +1,15 @@
+
+
+let country={
+
+    countryName:"pakistan",
+    moutain:"k2",
+    River:"jehlum",
+    language:"urdu",
+    provinces:5
+}
+  //console.log(country);
+
+   country["countryNear"]="soudiarabia";
+   country.pahari="siacheen";     
+   console.log(country);
