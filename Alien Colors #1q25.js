5b8c29ced2192e0c61ad401f18560e3ba17c430c
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-11-09 23:12:36 +0500

    Add files via upload

---
+++ Alien Colors #1q25.js
@@ -0,0 +1,33 @@
+let alien_color=["green","yellow","red"];
+points=0;
+message=0;
+if(alien_color="green"){
+    points=5;
+    message="player just earned 5 points";
+}else{
+points=10;
+message="player not earned any thing";
+}
+console.log(message);
+let alien_color1=["green","yellow","red"];
+points=0;
+message=0;
+if(alien_color1=="green"){
+    points=5;
+    message="player just earned 5 points";
+}else{
+points=10;
+message="player not earned any thing";
+}
+console.log(message);
+let alien_color2=["green","yellow","red"];
+points=0;
+message=0;
+if(alien_color2){
+    points;
+    message="";
+}else{
+points;
+message="";
+}
+console.log(message);
