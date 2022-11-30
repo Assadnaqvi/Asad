33143a044c1a354676c78692674e57142c58e85f
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-11-30 22:50:43 +0500

    Add files via upload

---
+++ Alien color27.js
@@ -0,0 +1,12 @@
+let alien_color=["green","yellow","red"];
+if(alien_color="green"){
+    message="player eared 5 points";
+    console.log(message);
+}if (alien_color="yellow"){
+    message="player earned 10 points"
+console.log(message);
+}if (alien_color="red"){
+    message="player earned 15 points"
+    
+}
+console.log(message);
