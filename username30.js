e8dbd1453aefe6c7691caa6fa4cf6eda99ac3aac
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-11-30 22:55:10 +0500

    Add files via upload

---
+++ username30.js
@@ -0,0 +1,6 @@
+let userName=["Admin"];
+if(userName="Admin"){
+    console.log("Hellow Admin,would you like to see status report");
+}else{
+    console.log("Hellow Eric,thank you for logging aagain");
+}
