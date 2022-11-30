b8d6a78f09e20d790fe7bc916c5fff552ae591a6
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-11-30 22:52:25 +0500

    Add files via upload

---
+++ Hellow Admin30a.js
@@ -0,0 +1,7 @@
+let HellowAdmin=["admin","Asad","Ali","Ejaz","Hussain"];
+message="hellow welcome to Panaverse";
+console.log(HellowAdmin[0],"!",message);
+console.log(HellowAdmin[1],"!",message);
+console.log(HellowAdmin[2],"!",message);
+console.log(HellowAdmin[3],"!",message);
+console.log(HellowAdmin[4],"!",message);
