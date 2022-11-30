b1ae9e976ba7186ad04b7ea43974d1e5e634fb76
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-11-30 22:49:10 +0500

    Add files via upload

---
+++ Alien color26a.js
@@ -0,0 +1,11 @@
+let alien_color=["green","yellow","red"];
+points=0;
+message=0;
+if(alien_color="green"){
+    points=5;
+    message="if alien color is green!player just earned 5 points for shooting the alien";
+}else{
+points=10;
+message="player not earned any thing";
+}
+console.log(message);
