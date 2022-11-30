a2917503736c0719061de405d6310252a940fcac
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-11-30 22:49:58 +0500

    Add files via upload

---
+++ Alien color 26c.js
@@ -0,0 +1,22 @@
+let alien_color=["green","yellow","red"];
+points=0;
+message=0;
+if(alien_color="green"){
+    points=5;
+    message="if alien color is green!player just earned 10";
+}else{
+points=10;
+message="player not earned any thing";
+}
+console.log(message);
+let alien_color1=["green","yellow","red"];
+points=0;
+message=0;
+if(alien_color!="green"){
+    points=5;
+    message="if alien color is green!player just earned 10";
+}else{
+points=10;
+message="player not earned any thing";
+}
+console.log(message);
