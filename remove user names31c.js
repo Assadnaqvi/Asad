0ba20ab3901491e79013ea2ed2c764e89161028b
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-11-30 22:54:48 +0500

    Add files via upload

---
+++ remove user names31c.js
@@ -0,0 +1,16 @@
+let users=["Admin","Asad","Ali","Ejaz","Hussain"];
+let user=users.pop();
+console.log(user);
+console.log(users);
+let user1=users.pop();
+console.log(user1);
+console.log(users);
+let user3=users.pop();
+console.log(user3);
+console.log(users);
+let user4=users.pop();
+console.log(user4);
+console.log(users);
+let user5=users.pop();
+console.log(user5);
+console.log(users);
