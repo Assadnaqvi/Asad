9a3f8cbbf94254716f3166f49f99f019446e4317
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-11-30 22:52:02 +0500

    Add files via upload

---
+++ Five if statements29.js
@@ -0,0 +1,14 @@
+let favouriteFruites=["mango","guava","grapes","banana","apple"];
+if(favouriteFruites="mango"){
+    console.log("my favourite fruite is mango");
+}if(favouriteFruites="guava"){
+    console.log("my favourite fruite is guava");
+}
+if(favouriteFruites="grapes"){
+    console.log("my favourite fruite is grapes");
+}
+if(favouriteFruites="banana"){
+    console.log("my favourite fruite is banana");
+}if(favouriteFruites="apple"){
+    console.log("my favourite fruite is apple");
+}
