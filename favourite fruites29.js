644702253258e6cd71ac7ce9c480991b7cd6381e
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-11-30 22:51:28 +0500

    Add files via upload

---
+++ favourite fruites29.js
@@ -0,0 +1,19 @@
+let favouriteFruites=["mango","guava","grapes","banana","apple","strawberry","blueberry"];
+if(favouriteFruites="mango"){
+    console.log("my favourite fruite is mango");
+}if(favouriteFruites="guava"){
+    console.log("my favourite fruite is guava");
+}
+if(favouriteFruites="grapes"){
+    console.log("my favourite fruite is grapes");
+}
+if(favouriteFruites="banana"){
+    console.log("my favourite fruite is banana");
+}if(favouriteFruites="apple"){
+    console.log("my favourite fruite is apple");
+}
+if(favouriteFruites="strawberry"){
+    console.log("my favourite fruite is strawberry");
+}if(favouriteFruites="blueberry"){
+    console.log("my favourite fruite is blueberry");
+}
