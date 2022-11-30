ff82a9ab5536ffe699c391cc722fd180f50b1bba
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-11-30 22:53:27 +0500

    Add files via upload

---
+++ person age28.js
@@ -0,0 +1,13 @@
+let age=0;
+if (age<2){
+    console.log("person is a baby");
+}else if(age=2&&age<4){
+    console.log("person is toddler");
+
+}else if(age=4&&age<13){
+    console.log("person is a kid");
+}else if(age=13&&age<20){
+    console.log("person is teenager");
+}else{
+    console.log("person is adult");
+}
