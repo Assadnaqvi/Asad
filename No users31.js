59300493d9dfe574ecdfa4d46bc0c7a0ae03d86a
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-11-30 22:53:08 +0500

    Add files via upload

---
+++ No users31.js
@@ -0,0 +1,14 @@
+let users=["Admin","Asad","Ali","Ejaz","Hussain"];
+let age=0;
+if (users="Admin"&&age<=2){
+    console.log("list is not empty");
+}else if(age=2&&age<4){
+    console.log("person is toddler");
+
+}else if(age=4&&age<13){
+    console.log("person is a kid");
+}else if(age=13&&age<20){
+    console.log("person is teenager");
+}else{
+    console.log("person is adult");
+}
