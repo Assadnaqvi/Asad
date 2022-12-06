ab3605b544879078d4fb0365742b986e9e0fcb2a
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-12-06 17:38:42 +0500

    Add files via upload

---
+++ function43.js
@@ -0,0 +1,22 @@
+function make_album(){
+    console.log(object);
+    
+    }
+    let object={
+          songs:"folk",
+          name:"nusrat",
+          name1:"abda",
+          name2:"alizafar"
+    
+    }
+    make_album();function show_magician(){
+
+        console.log(push.array);
+    //array2.push(array);
+
+    }
+    
+    let array=["david","alibaba","harvis","notoro"];
+    let array2=[]
+    show_magician()
+    
