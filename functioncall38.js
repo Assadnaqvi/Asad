369b5f8640b655abcc7a689edc352835588bba80
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-12-06 17:39:47 +0500

    Add files via upload

---
+++ functioncall38.js
@@ -0,0 +1,13 @@
+function describe_city(pakistan){
+return pakistan;
+    console.log("karachi is in pakistan");
+}
+
+//const karachi="karachi is in pakistan"
+//describe_city(pakistan);
+//describe_city("karachi");
+let result=describe_city("kaachi is in pakistan");
+let country=describe_city("lahore is in pakistan");
+console.log(result);
+console.log(country);
+describe_city(pakistan);
