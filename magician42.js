cff276b90988fc20b7bfe5e432ba4bffe25f5f46
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-12-06 17:40:31 +0500

    Add files via upload

---
+++ magician42.js
@@ -0,0 +1,15 @@
+function show_magician(){
+for(i=0;i<array.length;i++){
+    console.log("these are great magicians!=="+array);
+
+}}
+let array=["david","alibaba","harvis","notoro"];
+function city_country(city,country){
+
+    return city , country;
+}
+console.log(`"Lahore,Pakistan"`);
+console.log(`"karachi,Pakistan"`);
+console.log(`"Islamabad,Pakistan"`);
+
+show_magician()
