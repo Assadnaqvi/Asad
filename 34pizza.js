8b1e8943b678d1dadf95d704763a64ab8ff43db7
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-12-06 17:36:29 +0500

    Add files via upload

---
+++ 34pizza.js
@@ -0,0 +1,7 @@
+
+let pizza=["fajita","tikka","peproni"];
+for(let i=0;i<pizza.length;i++){
+    console.log(pizza[i]);}
+console.log("i like pizza very much");
+console.log("i like very much!"+pizza[2]+">pizza");
+
