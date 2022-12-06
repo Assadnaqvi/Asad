dbb9a8d4516b045b6d1a35a12f50f7987e63e337
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-12-06 17:36:11 +0500

    Add files via upload

---
+++ 33 question.js
@@ -0,0 +1,13 @@
+let i=0;
+let array=[];
+for(let i=1;i<2;i++){
+    console.log(i+"st");
+    for(let i=2;i<3;i++){
+        console.log(i+"nd");
+        for(let i=3;i<4;i++){
+            console.log(i+"rd");
+for(let i=4;i<10;i++){
+  console.log(i+"th");
+}
+
+}}}
