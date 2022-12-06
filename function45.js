953694515b687663a7883fcd47f9e1822e05cf8d
Syed Asid Maqsood Nakvi <110187311+Assadnaqvi@users.noreply.github.com>
2022-12-06 17:39:31 +0500

    Add files via upload

---
+++ function45.js
@@ -0,0 +1,23 @@
+function car(model,color){
+
+    console.log(object,object1);
+
+}
+let object={
+    manufacturer:"honda",
+    model:1990
+
+
+
+}
+let object1={
+    color:"red",
+speedmax:200+"miles/h"
+
+}
+
+
+
+
+
+car(object,object1);
